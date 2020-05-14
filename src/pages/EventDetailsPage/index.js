22070799503ada3bb880467cbf25f5c38ac6ce77
Thai <ttgilles@ucsd.edu>
2020-05-14 16:40:48 -0700

    Made changes to how EventDetailsComponent looks

--- src/pages/EventDetailsPage/index.js
+++ src/pages/EventDetailsPage/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Loading from '../../components/Loading';
+
 import EventDetailsComponent from '../../components/EventDetails';
 import { getEventById } from '../../services/events';
 
@@ -32,7 +34,7 @@ class EventDetailsPage extends React.Component
 
   render()
   {
-    const EventDetails = (this.state.eventInfo === null) ? <div></div> : <EventDetailsComponent eventInfo={this.state.eventInfo} />
+    const EventDetails = (this.state.eventInfo === null) ? <Loading /> : <EventDetailsComponent eventInfo={this.state.eventInfo} />
 
     return (
       EventDetails
