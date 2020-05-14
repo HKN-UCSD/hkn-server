5dc31e700c7d5557b963fa5e57672de5082e688f
gypang <gypang24@gmail.com>
2020-05-14 14:12:25 -0700

    Changes context to pass down object instead of just the keys.

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -71,7 +71,7 @@ class App extends React.Component {
 
         ClaimsSingleton.setClaims(claims);
         this.setState({
-          authUserClaims: Object.keys(claims),
+          authUserClaims: claims,
           isLoading: false,
         });
       } else {
