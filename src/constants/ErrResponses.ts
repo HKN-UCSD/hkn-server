4019ff7a03130fb72d63e42a89545b3764548298
AneeshJalan <akjalan@ucsd.edu>
2020-07-02 03:13:19 +0530

    Added signup endpoint to server.
    Created an Auth controller and router that exposes and handles the /signup endpoint.
    Also created a file containing some constant strings for use in sending back error
    response messages.
    Added signup endpoint to server.
    
    Created an Auth controller and router that exposes and handles the /signup endpoint.
    Also created a file containing some constant strings for use in sending back error
    response messages.

---
+++ src/constants/ErrResponses.ts
@@ -0,0 +1,5 @@
+export const GENERIC_400_MSG = "The request object received had invalid syntax or missing information.";
+export const USER_NOT_WHITELISTED = "Cannot create an account since the email id provided does not have any affiliation with HKN. Visit hkn.ucsd.edu to know more.";
+export const USER_ALREADY_EXISTS = "The provided email is aready linked to an account. Please check the email you have entered.";
+export const FIREBASE_AUTH_CREATE_USER_ERR = "There was an internal error creating an auth account. Please try again later.";
+export const FIRESTORE_UPDATE_DOC_ERR = "There was an error modifying the user document. Please try again later."
