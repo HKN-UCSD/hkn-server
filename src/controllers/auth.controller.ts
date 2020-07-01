4cf285dbe205ac894c5aedff3d715d5a32b54984
AneeshJalan <akjalan@ucsd.edu>
2020-07-02 03:13:19 +0530

    Added signup endpoint to server. Amends: 1.
    Created an Auth controller and router that exposes and handles the /signup endpoint.
    Also created a file containing some constant strings for use in sending back error
    response messages.
    
    Amend:
    Addressed suggestions in the PR. Added functionality to send account verification
    emails and also set auth claims for users based on their roles.

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,186 @@
+import admin from 'firebase-admin';
+import { Request, Response, NextFunction } from 'express';
+import { firebase as client } from '@firebase/app';
+import * as ERR_MSG from '../constants/ErrResponses';
+
+
+/*
+  Create a new auth object for the provided email and password.
+  Also links the auth account with existing user document and
+  updates document with provided profile information.
+  Prereqs:
+    1.  Request body contains the following key value pairs:
+        a. email
+        b. password
+        c. firstname
+        d. lastname
+        e. major
+        f. gradYear
+  Return: On successful account creation, user document is updated with incoming profile
+          information and user auth account object is sent back mapped to newUser.
+  Errors:`Can send back the following error responses: 400, 403, 500.
+*/
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+
+
+  if (!validInput(req.body)) {
+    // Invalid user information
+    res.status(400).json({
+      msg: ERR_MSG.GENERIC_400_MSG,
+    });
+    return next();
+  }
+
+  // Grab form information from request body
+  const { email,
+    password,
+    firstname,
+    lastname,
+    major,
+    gradYear } = req.body;
+
+  const userDocDetails: any = await getUserDocFromEmail(email);
+  const userDocID: string = userDocDetails.uid;
+
+  if (!userDocDetails) {
+    // email is not whitelisted.
+    res.status(403).json({
+      msg: ERR_MSG.USER_NOT_WHITELISTED,
+    });
+    return next();
+  }
+
+  let newUser: admin.auth.UserRecord;
+
+  try {
+    newUser = await admin.auth().createUser({
+      uid: userDocID,
+      email: email,
+      password: password
+    });
+
+    const updatedClaims: any = newUser.customClaims || {};
+    console.log("Initial User claims:");
+    console.log(newUser.customClaims);
+
+    // claims are either 'member', 'inductee' or 'officer
+    updatedClaims[userDocDetails.role.toLowerCase()] = true;
+    console.log("Updated User Claims:")
+    console.log(updatedClaims);
+    await admin.auth().setCustomUserClaims(userDocID, updatedClaims).catch(console.log);
+  }
+  catch (err) {
+    console.log(err);
+    if (err.code == 'auth/email-already-exists' || err.code == 'auth/uid-already-exists') {
+      // Account already exists.
+      res.status(403).json({
+        msg: ERR_MSG.USER_ALREADY_EXISTS,
+      });
+      console.log("account already exists");
+      return next();
+    }
+    else {
+      res.status(500).json({
+        msg: ERR_MSG.GENERIC_INTERNAL_ERROR,
+      });
+      console.log("Internal server error while creating auth");
+      return next();
+    }
+  }
+
+  // if (!newUser) {
+  //   return next();
+  // }
+
+  try {
+    await updateUserDoc('users', userDocID, {
+      first_name: firstname,
+      last_name: lastname,
+      major: major,
+      graduation_year: gradYear,
+    });
+
+    // Sign in to user account, send verification email and sign out.
+    await signInWithEmailAndPass(email, password);
+    await sendAccountVerificationEmail();
+    await signOut();
+
+    res.status(200).json({
+      userRecord: newUser,
+    });
+
+    return next();
+  }
+  catch (err) {
+    console.log(err);
+    // Could not update the user document with provided data.
+    // Delete account because we want to have this data in the user account at all times.
+    admin.auth().deleteUser(userDocID);
+
+    res.status(500).json({
+      msg: ERR_MSG.GENERIC_INTERNAL_ERROR,
+    });
+    return next();
+  }
+};
+
+function validInput(
+  body: any
+): boolean {
+
+  // Grab form information from request body or if body is undefined,
+  // parse from {}. (Helps avoid TypeError)
+  const { email,
+    password,
+    firstname,
+    lastname,
+    major,
+    gradYear } = body || {};
+
+  // Add form input validation here.
+  if (!email || !password || !firstname || !lastname || !major || !gradYear) {
+    return false;
+  }
+
+  return true;
+}
+
+async function getUserDocFromEmail(email: string): Promise<any> {
+  return admin.firestore().collection('users')
+    .where('email', '==', email)
+    .get()
+    .then(snapshot => {
+      if (snapshot.empty) {
+        return undefined;
+      }
+      const userData = snapshot.docs[0].data();
+      userData.uid = snapshot.docs[0].id;
+      return userData;
+    });
+}
+
+async function updateUserDoc(collection: string, id: string, data: any) {
+  return admin.firestore()
+    .collection(collection)
+    .doc(id)
+    .update(data);
+}
+
+async function signInWithEmailAndPass(
+  email: string,
+  password: string
+) {
+  await client.auth().signInWithEmailAndPassword(email, password);
+}
+
+async function signOut() {
+  await client.auth().signOut();
+}
+
+function sendAccountVerificationEmail() {
+  client.auth().currentUser.sendEmailVerification();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import express from 'express';
 import admin from 'firebase-admin';
+import { firebase as client } from '@firebase/app';
+import '@firebase/auth';
 import { config } from './config';
 import { UserRouter } from './routers/user.router';
 import { DocsRouter } from './routers/docs.router';
+import { AuthRouter } from './routers/auth.router';
+
 
 admin.initializeApp({
   credential: admin.credential.cert(config.firebaseConfig),
   databaseURL: config.dbURL,
+});
+
+
+console.log(config.clientApiKey)
+console.log(config.clientAppID)
+console.log(config.firebaseConfig.project_id)
+
+client.initializeApp({
+  apiKey: config.clientApiKey,
+  projectId: config.firebaseConfig.project_id,
+  appId: config.clientAppID,
 });
 
 const app = express();
@@ -17,5 +32,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/user', UserRouter);
 app.use('/docs', DocsRouter);
+app.use('/api/auth', AuthRouter);
 
 app.listen(port);
