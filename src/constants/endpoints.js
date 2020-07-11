cac057e83404802a10bfeb3e1d88e2ab06cec5d7
thaigillespie <ttgilles@ucsd.edu>
2020-07-10 22:14:06 -0700

    Added endpoints.js to store endpoint constants for API fetch calls.
    endpoints.js is in src/constants. Since I made this branch from master,
    there isn't a place where I can use the API's base url (an environment
    variable) with an endpoint constant to make a fetch call(), so yeah
    this is gonna be a short PR (I can do this in newSignupPage branch,
    but the PR for that is already so bloated I don't want to add more).
    
    And also, to be frank, the biggest change for this PR is in .env, so
    that's also why this is such a short PR.

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,3 +1,6 @@
-const SIGNUP_ENDPOINT = '/api/auth/signup';
-
-export default SIGNUP_ENDPOINT;
+/*
+ * Endpoint constants for API
+ * TODO: Remove PLACEHOLDER when adding more API endpoints
+ */
+export const SIGNUP_ENDPOINT = '/api/auth/signup';
+export const PLACEHOLDER = '';
