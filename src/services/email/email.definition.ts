0960f4d333b79de8fd7a314c1b0ff581cdd045d5
Thai <ttgilles@ucsd.edu>
2020-07-24 01:29:45 -0700

    Created custom TS interface for the EmailInfo objects of emailInfoArr.
    Now, the service consists of a one-line private function that sends
    one or more emails to one or more respective recipients for each
    of those emails.

---
+++ src/services/email/email.definition.ts
@@ -0,0 +1,9 @@
+export interface EmailInfo {
+  to: Array<string>;
+  from: string;
+  dynamicTemplateData: { [key: string]: any };
+  templateId: string;
+  cc?: Array<string>;
+  bcc?: Array<string>;
+  sendAt?: number;
+}
