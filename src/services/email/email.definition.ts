39d45c7e6db59e192e4d9a8f468b3f67e46ec8d9
Thai <ttgilles@ucsd.edu>
2020-07-24 16:37:20 -0700

    Made changes to email service so users won't need to choose a sender.
    Created a separate type in email.definition.ts called EmailInfoRequired
    that combines EmailInfo
    and 'from' property to conform to MailDataRequired type of SendGrid's
    send().
    Made a function that inserts 'from' fields into EmailInfo objects and
    returns them as EmailInfoRequired objects
    
    Added a public method (sendEmail()) that sends an email to one or more
    people.
    
    Note that in the EmailInfo interface, 'from' property is still defined,
    but is now optional.

--- src/services/email/email.definition.ts
+++ src/services/email/email.definition.ts
@@ -1,9 +1,11 @@
 export interface EmailInfo {
   to?: Array<string>;
-  from: string;
+  from?: string;
   dynamicTemplateData: { [key: string]: any };
   templateId: string;
   cc?: Array<string>;
   bcc?: Array<string>;
   sendAt?: number;
 }
+
+export type EmailInfoRequired = EmailInfo & { from: string };
