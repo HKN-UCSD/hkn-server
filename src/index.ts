11bf610eeaf98fb73d94c804c66bb43d6270a736
Thai <ttgilles@ucsd.edu>
2020-07-26 14:45:44 -0700

    Added rate limiting to all endpoints so we don't get DDOS-ed.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import cors from 'cors';
+import 'express-rate-limit';
+
 import admin from 'firebase-admin';
 import { firebase as client } from '@firebase/app';
 import '@firebase/auth';
+
 import { config } from './config';
 import { UserRouter } from './routers/user.router';
 import { DocsRouter } from './routers/docs.router';
 import { AuthRouter } from './routers/auth.router';
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: parseInt(process.env.RATE_LIMIT_TIMEFRAME, 10),
+  max: parseInt(process.env.RATE_LIMIT_MAX_CONNS, 10),
+});
 
 admin.initializeApp({
   credential: admin.credential.cert(config.firebaseConfig),
@@ -25,6 +34,7 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(limiter);
 
 app.use('/api/user', UserRouter);
 app.use('/docs', DocsRouter);
