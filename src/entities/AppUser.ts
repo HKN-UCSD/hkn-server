ca584687e1bf9d52ec038d08f5476a3a4358e6b8
AneeshJalan <akjalan@ucsd.edu>
2020-08-26 12:27:48 +0530

    Removed JoinTable as not needed for ManyToOne relations.

--- src/entities/AppUser.ts
+++ src/entities/AppUser.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinTable } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 
 import { InductionClass } from './InductionClass';
 
@@ -37,7 +37,6 @@ export class AppUser {
   graduationYear: string;
 
   @ManyToOne(() => InductionClass, { nullable: true, eager: true })
-  @JoinTable()
   inductionClass: InductionClass;
 
   @Column({
