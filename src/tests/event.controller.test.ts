1db2a8d168a019459679635a1af6c44744f09a68
Thai <ttgilles@ucsd.edu>
2020-08-28 02:20:29 -0700

    Test file for making requests to event endpoints now works properly.
    The test file also serves as an example of how to do the preparation
    steps and the cleanup steps after all tests are run, at least for
    end-to-end tests for our API endpoints

--- src/tests/event.controller.test.ts
+++ src/tests/event.controller.test.ts
@@ -1,11 +1,47 @@
 import request from 'supertest';
-import { getExpressApp } from '../index';
+import { getExpressApp } from '../app';
+import { Connection } from 'typeorm';
 
-describe('GET /events', () => {
-  it('tests if GET request to /events can be made without crashing the server.', async () => {
-    const app = await getExpressApp();
+class currentApp {
+  private app: any;
+  private connection: Connection;
+
+  set(app: any, connection: Connection): void {
+    this.app = app;
+    this.connection = connection;
+  }
+
+  get() {
+    return { app: this.app, connection: this.connection };
+  }
+}
+
+const currApp = new currentApp();
+
+beforeAll(async () => {
+  const { app, connection } = await getExpressApp();
+  currApp.set(app, connection);
+});
+
+afterAll(async () => {
+  const { connection } = currApp.get();
+  await connection.close();
+});
+
+describe('GET /api/events', () => {
+  it('tests if GET request to /api/events can be made without crashing the server.', async () => {
+    const { app } = currApp.get();
     const res = await request(app).get('/api/events');
 
-    expect(res.status).toBe(200);
+    expect(res.status).toEqual(200);
+  });
+});
+
+describe('GET /api/events/:eventID', () => {
+  it('tests if GET request to /api/events/:eventID with some hardcoded eventID can be made without crashing the server.', async () => {
+    const { app } = currApp.get();
+    const res = await request(app).get('/api/events/2');
+
+    expect(res.status).toEqual(200);
   });
 });
