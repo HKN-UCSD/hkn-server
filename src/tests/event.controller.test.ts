493355fcf2e562766fb4008df3e5c94536e0902a
Thai <ttgilles@ucsd.edu>
2020-08-28 09:47:35 -0700

    Cleaned up the setup and teardown steps of the jest tests.

--- src/tests/event.controller.test.ts
+++ src/tests/event.controller.test.ts
@@ -2,36 +2,23 @@ import request from 'supertest';
 import { getExpressApp } from '../app';
 import { Connection } from 'typeorm';
 
-class currentApp {
-  private app: any;
-  private connection: Connection;
-
-  set(app: any, connection: Connection): void {
-    this.app = app;
-    this.connection = connection;
-  }
-
-  get() {
-    return { app: this.app, connection: this.connection };
-  }
-}
-
-const currApp = new currentApp();
+let testApp: any = null;
+let testConnection: Connection = null;
 
 beforeAll(async () => {
   const { app, connection } = await getExpressApp();
-  currApp.set(app, connection);
+
+  testApp = app;
+  testConnection = connection;
 });
 
 afterAll(async () => {
-  const { connection } = currApp.get();
-  await connection.close();
+  await testConnection.close();
 });
 
 describe('GET /api/events', () => {
   it('tests if GET request to /api/events can be made without crashing the server.', async () => {
-    const { app } = currApp.get();
-    const res = await request(app).get('/api/events');
+    const res = await request(testApp).get('/api/events');
 
     expect(res.status).toEqual(200);
   });
@@ -39,8 +26,7 @@ describe('GET /api/events', () => {
 
 describe('GET /api/events/:eventID', () => {
   it('tests if GET request to /api/events/:eventID with some hardcoded eventID can be made without crashing the server.', async () => {
-    const { app } = currApp.get();
-    const res = await request(app).get('/api/events/2');
+    const res = await request(testApp).get('/api/events/2');
 
     expect(res.status).toEqual(200);
   });
