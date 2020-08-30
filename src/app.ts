c37b2371797743a8c41456905cc82943f9e0ee96
Thai <42761684+thaigillespie@users.noreply.github.com>
2020-08-29 20:15:17 -0700

    Testing Epic - (1) End-to-end tests for our API endpoints (#79)
    * Trying out testing with jest.
    
    * Test file for making requests to event endpoints now works properly.
    
    The test file also serves as an example of how to do the preparation
    steps and the cleanup steps after all tests are run, at least for
    end-to-end tests for our API endpoints
    
    * Cleaned up the setup and teardown steps of the jest tests.
    
    * Added type for testApp in jest tests.
    
    * Update jest to only run ts files.
    
    Co-authored-by: gypang <gypang24@gmail.com>

---
+++ src/app.ts
@@ -0,0 +1,55 @@
+import 'reflect-metadata'; // shim required for routing-controllers
+import 'module-alias/register'; // required for aliases
+import express from 'express';
+import rateLimit from 'express-rate-limit';
+import compression from 'compression';
+
+import { UserRouter } from './routers/UserRouter';
+import { DocsRouter } from './routers/DocsRouter';
+import { AuthRouter } from './routers/AuthRouter';
+
+import { useExpressServer, useContainer as routingUseContainer } from 'routing-controllers';
+import { controllers, ControllerContainer } from './controllers';
+
+import { loadFirebase, loadORM } from './loaders';
+import morgan from 'morgan';
+
+import { checkCurrentUserToken } from './decorators';
+
+const limiter = rateLimit({
+  windowMs: parseInt(process.env.RATE_LIMIT_TIMEFRAME, 10),
+  max: parseInt(process.env.RATE_LIMIT_MAX_REQUESTS, 10),
+});
+
+export const getExpressApp = async () => {
+  const connection = await loadORM();
+  loadFirebase();
+
+  const app = express();
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(morgan('tiny'));
+  app.use(limiter);
+
+  // load controllers; maybe move into loader?
+
+  // tell routing-controllers to use typedi container
+  routingUseContainer(ControllerContainer);
+  useExpressServer(app, {
+    cors: true,
+    controllers: controllers,
+    currentUserChecker: checkCurrentUserToken,
+  });
+
+  app.use(compression());
+
+  app.use('/api/docs', DocsRouter);
+
+  // following two routers will be deprecated and moved into
+  // routing-controllers
+  //app.use('/api/user', UserRouter);
+  //app.use('/api/auth', AuthRouter);
+
+  return { app, connection };
+};
