322088a655b740bc415ea3c3f9183c098d0bf251
Thai <42761684+thaigillespie@users.noreply.github.com>
2020-08-31 00:01:15 -0700

    Added most of the user endpoints covered in the issues (#84)
    * Added most of the user endpoints covered in the issues, still not done.
    
    Still has GET on /api/users?officers=true&names=true to do.
    
    * Added query params to GET on /api/users endpoint. Ready for review.
    
    * Making changes based on review from PR #84 (WIP)
    
    * Still WIP
    
    * Added most (if not all) of the changes addressed and discussed in the
    current review of the PR for this.
    
    * Added TODO comment.

--- src/payloads/index.ts
+++ src/payloads/index.ts
@@ -7,10 +7,15 @@ export {
 } from './Event';
 export {
   AppUserPKPayload,
+  AppUserPostRequest,
   AppUserEventRequest,
+  AppUserResponse,
   AppUserEventResponse,
   AppUserRolesResponse,
   AppUserProfileResponse,
+  MultipleUserQuery,
+  MultipleAppUserResponse,
+  MultipleUserNameResponse,
 } from './AppUser';
 export { AttendanceResponse } from './Attendance';
 export { RSVPResponse } from './RSVP';
