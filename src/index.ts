84282375887ed60db8c8f395bc45c81ad22e7c23
Godwin Pang <gypang24@gmail.com>
2020-09-05 12:57:29 -0700

    Revert "Add ssl cert challenge." (#93)
    This reverts commit c89140661b31783fa923ed196a45cdbc221893bf.

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,6 @@ const port = process.env.PORT || 3001;
 
 getExpressApp()
   .then(({ app }) => {
-    app.get('/.well-known/acme-challenge/:stuff', function(req, res) {
-      res.send(
-        'lY5ekOW4V3J5Md_RI-p6BKhm4S4wGH_y4aHa9ZTHhM8.XSTt1MkFj-yk3jqAG1v3Bk6iRbzyc_NsGWoLKXSvxqE'
-      );
-    });
     app.listen(port);
   })
   .catch(err => err);
