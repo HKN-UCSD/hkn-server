4ba6206ea2fc6d3477cd0a09483f1472634e6041
Godwin Pang <gypang24@gmail.com>
2020-09-15 23:21:12 -0700

    Fix user stat view (#102)
    * Remove NODE_ENV from npm script.
    
    * Remove TODO.
    
    * Add views.
    
    * Add more things to original seed to test view.
    
    * Add migration for view.

---
+++ src/entities/MemberPointsView.ts
@@ -0,0 +1,26 @@
+import { ViewEntity, Connection, ViewColumn } from 'typeorm';
+import { AppUser } from './AppUser';
+import { Attendance } from './Attendance';
+
+// later do some grouping by quarter
+// honestly we could maybe make this a query
+// idk we'll figure it out someday
+// - Godwin Sept 12th 2021 :sadge:
+@ViewEntity({
+  expression: (connection: Connection) =>
+    connection
+      .createQueryBuilder()
+      .select('appUser.id', 'user')
+      .addSelect('SUM(attendance.duration)', 'points')
+      .from(AppUser, 'appUser')
+      .innerJoin(Attendance, 'attendance', 'appUser.id = attendance.attendee')
+      .groupBy('appUser.id')
+      .where('NOT attendance.isInductee'),
+})
+export class MemberPointsView {
+  @ViewColumn()
+  user: AppUser;
+
+  @ViewColumn()
+  points: number;
+}
--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,6 +1,8 @@
 export { AppUser, AppUserRole } from './AppUser';
 export { Attendance } from './Attendance';
 export { Event, EventStatus, EventType } from './Event';
-export { InducteeStat } from './InducteeStat';
 export { InductionClass } from './InductionClass';
 export { RSVP } from './RSVP';
+
+export { MemberPointsView } from './MemberPointsView';
+export { InducteePointsView } from './InducteePointsView';
