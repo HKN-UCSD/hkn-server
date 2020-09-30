281b61d38224891efbfb168126fe14786792dbfc
Thai <ttgilles@ucsd.edu>
2020-09-30 13:46:24 -0700

    Remove extraneous console.log statements

--- src/services/email/email.service.ts
+++ src/services/email/email.service.ts
@@ -5,9 +5,6 @@ import { EmailInfo } from './email.definition';
 SendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
 const senderEmail: string = process.env.SENDER_EMAIL_ADDRESS;
 
-console.log(process.env.SENDGRID_API_KEY);
-console.log(process.env.SENDGRID_EMAIL_ADDRESS);
-
 /*
  * Send one or more emails to one or more respective recipients
  * for each of those emails
