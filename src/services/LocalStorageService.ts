38e3cea4e1bf1c8f4676ed5b8a04c0bdfbf571fa
Eric Ke <keric.yi@gmail.com>
2021-02-12 21:16:14 -0800

    S3 eco (#170)
    add endpoints and services for uploading and downloading resumes specific to each user into S3 buckets

---
+++ src/services/LocalStorageService.ts
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import { Response } from 'express';
+
+const dir_path = process.cwd() + '/local_fs/';
+
+if (!fs.existsSync(dir_path)) {
+  fs.mkdirSync(dir_path);
+}
+
+export class LocalStorageService {
+  async uploadFile(fileName: string, file: Express.Multer.File, options?: Object): Promise<string> {
+    let fileNameKey = fileName;
+    if (options) {
+      if ('appendFileName' in options) {
+        fileNameKey = `${fileName}_${options['appendFileName']}`;
+      }
+    }
+
+    fs.writeFile(
+      dir_path + fileNameKey + file.originalname.substring(file.originalname.lastIndexOf('.')),
+      file.buffer,
+      function(err) {
+        if (err) {
+          throw new Error(`Could not write file to ${dir_path + fileName}: ${err.message}`);
+        }
+      }
+    );
+    return `Uploaded file to ${dir_path + fileName}`;
+  }
+
+  async downloadFile(fileName: string, res: Response): Promise<Buffer | null> {
+    try {
+      res.set({
+        'content-disposition': `attachment; filename="${fileName}"`,
+      });
+      return fs.promises.readFile(dir_path + fileName);
+    } catch (e) {
+      throw new Error(`Could not retrieve file from fs at ${dir_path + fileName}: ${e.message}`);
+    }
+  }
+}
+
+export const LocalStorageServiceImpl = new LocalStorageService();
