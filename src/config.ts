e48b6ab94a393ace89442f3a6a3d6a35fa21f126
gypang <gypang24@gmail.com>
2021-02-13 20:33:04 -0800

    Revert "S3 eco (#170)"
    This reverts commit 20931a582bc215eeedd3a8c7b289b6433416d2cd.

--- src/config.ts
+++ src/config.ts
@@ -4,16 +4,8 @@ type FirebaseConfig = {
   privateKey: string;
 };
 
-type AWSConfig = {
-  accessKeyId: string;
-  secretKey: string;
-  bucketName: string;
-  bucketRegion: string;
-};
-
 type Config = {
   firebaseConfig: FirebaseConfig;
-  awsConfig: AWSConfig;
   dbURL: string;
   clientAppID: string;
   clientApiKey: string;
@@ -29,10 +21,6 @@ const {
   FIREBASE_DATABASE_URL,
   FIREBASE_CLIENT_ID,
   FIREBASE_CLIENT_API_KEY,
-  AWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY,
-  BUCKET_NAME,
-  BUCKET_REGION,
   DEV_AUTH,
   DD_METRIC_TAG,
   NODE_ENV,
@@ -44,16 +32,8 @@ const firebaseConfig: FirebaseConfig = {
   privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 };
 
-const awsConfig: AWSConfig = {
-  accessKeyId: AWS_ACCESS_KEY_ID,
-  secretKey: AWS_SECRET_ACCESS_KEY,
-  bucketName: BUCKET_NAME,
-  bucketRegion: BUCKET_REGION,
-};
-
 export const config: Config = {
   firebaseConfig: firebaseConfig,
-  awsConfig: awsConfig,
   dbURL: FIREBASE_DATABASE_URL,
   clientAppID: FIREBASE_CLIENT_ID,
   clientApiKey: FIREBASE_CLIENT_API_KEY,
