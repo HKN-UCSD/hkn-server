1e9bcba94f4dceacbb5222193c9f01c34c8a6f97
Godwin Pang <gypang24@gmail.com>
2021-03-13 11:16:40 -0800

    Revert "Monorepo frontend migration (#175)" (#203)
    This reverts commit 9f41435ee3c94b2255b7a630bf972c27d0cabffa, taking all out all frontend code.

--- frontend/src/pages/EventCreationPage/styles.ts
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@material-ui/core';
-
-const useStyles = makeStyles({
-  root: {
-    padding: '16px',
-  },
-});
-
-export default useStyles;
--- frontend/src/services/api/apis/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './AuthApi';
-export * from './EventApi';
-export * from './InductionClassApi';
-export * from './PointsApi';
-export * from './TeapotApi';
-export * from './UserApi';
