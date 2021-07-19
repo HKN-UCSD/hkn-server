c9a4dc79f280867ed3d15fb567379b25fa9f3bf7
yuw006 <yuw006@ucsd.edu>
2021-07-18 19:30:38 -0700

    add interface

--- frontend/src/components/alert/snackBar.tsx
+++ frontend/src/components/alert/snackBar.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 import Fade from '@material-ui/core/Fade';
+import { StringLiteralLike } from 'typescript';
+
+interface snackbarProps {
+  open: boolean;
+  message: string;
+  severity: string;
+  handleClose: undefined;
+}
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-const SnackbarAlerts = props => {
+const SnackbarAlerts = (props: snackbarProps) => {
   const { open, message, severity, handleClose } = props;
   return (
     <Snackbar
