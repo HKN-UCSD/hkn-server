97fa5111c4d238542377ed45b72f78f7ebc519e3
Audreyaaa <57417548+audreygao@users.noreply.github.com>
2021-08-06 22:04:46 -0700

    revert changes

--- frontend/src/services/AttendanceService.ts
+++ frontend/src/services/AttendanceService.ts
@@ -1,21 +1,16 @@
-import {
-  AttendanceApi,
-  AttendanceControllerDeleteAttendanceRequest,
-} from './api/apis/AttendanceApi';
+import { AttendanceApi, AttendanceControllerDeleteAttendanceRequest } from './api/apis/AttendanceApi';
 import { AttendanceResponse } from './api/models';
+
 import ApiConfigStore from './ApiConfigStore';
 import { Configuration } from './api/runtime';
 
-export async function deleteAttendance(
-  attendeeId: number,
-  eventId: number
-): Promise<AttendanceResponse> {
+export async function deleteAttendance(attendeeId: number, eventId: number): Promise<AttendanceResponse> {
   const apiConfig: Configuration = await ApiConfigStore.getApiConfig();
   const attendanceApi: AttendanceApi = new AttendanceApi(apiConfig);
   const request: AttendanceControllerDeleteAttendanceRequest = {
     attendeeId,
     eventId,
-  };
+  }
 
   return attendanceApi.attendanceControllerDeleteAttendance(request);
 }
