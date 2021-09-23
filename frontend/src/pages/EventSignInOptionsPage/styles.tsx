0cf81d3d659c45ebe22469f3e129e0281e8f0a00
Audreyaaa <57417548+audreygao@users.noreply.github.com>
2021-09-23 01:26:28 -0700

    Change event signin page to fit in phone (#274)
    * change event signin page to fit in phone
    
    * remove unnecessary lines

--- frontend/src/pages/EventSignInOptionsPage/styles.tsx
+++ frontend/src/pages/EventSignInOptionsPage/styles.tsx
@@ -1,22 +1,38 @@
 import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
+  main: {
+    alignItems: 'center',
+    [theme.breakpoints.down('sm')]: {
+      width: 100,
+    },
+  },
   buttonsDiv: {
     display: 'flex',
     flexWrap: 'wrap',
     flexDirection: 'column',
+    marginTop: '200px',
     marginLeft: 'auto',
     marginRight: 'auto',
     alignItems: 'center',
+    [theme.breakpoints.down(600)]: {
+      width: 300,
+      marginTop: '150px',
+    },
   },
   button: {
-    marginBottom: '30px',
+    marginBottom: '50px',
   },
   h2: {
+    textAlign: 'center',
     fontFamily: 'Roboto',
     color: '#000000',
     marginBottom: '60px',
+    [theme.breakpoints.down(600)]: {
+      fontSize: '20px',
+      marginBottom: '40px',
+    },
   },
-});
+}));
 
 export default useStyles;
