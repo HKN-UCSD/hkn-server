98723f552279f8ffb22d13974ccced77bc5cbfed
Tim Jiang <timstjiang@gmail.com>
2021-09-30 11:03:45 -0700

    Updated Info session times

--- frontend/src/constants/descriptions.tsx
+++ frontend/src/constants/descriptions.tsx
@@ -5,8 +5,8 @@ import { Link } from '@SharedComponents';
 
 export const DescriptionText = () => (
   <Typography>
-    Info Session 1: Friday, Oct. 8th at 4PM <br />
-    Info Session 2: Tuesday, Oct. 12th at 7PM <br />
+    Info Session 1: Friday, Oct. 8th at 7PM <br />
+    Info Session 2: Tuesday, Oct. 12th at 4PM <br />
 	Zoom Link: <Link href='https://ucsd.zoom.us/j/94709541216' openInNewTab /> <br />
   </Typography>
 );
