c4d6babfdb6d78f21ca437eee61047866aae932e
Tim Jiang <timstjiang@gmail.com>
2021-09-30 00:38:52 -0700

    typography

--- frontend/src/constants/descriptions.tsx
+++ frontend/src/constants/descriptions.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+ 
 
 export const DescriptionText = () => (
-  <div>
+  <Typography>
     Info Session 1: Friday, Oct. 8th at 4PM <br />
     Info Session 2: Tuesday, Oct. 12th at 7PM <br />
-  </div>
+  </Typography>
 );
