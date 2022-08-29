6306e02a2832c24f791b65ad43c075dac83f775f
Eric Hsiao <ehsiao789@gmail.com>
2022-08-28 19:28:04 -0700

    Revert "1 call to api for full name"
    This reverts commit 7f7f8ff3e0666416b56d3beac72353735b0b26bb.

--- frontend/src/services/PointsService.ts
+++ frontend/src/services/PointsService.ts
@@ -1,12 +1,10 @@
 import { PointsApi } from './api/apis/PointsApi';
 import ApiConfigStore from './ApiConfigStore';
 import { InducteePointsResponse } from './api';
-import { getUserById } from './UserService';
 
 // nit: bad naming
 export interface InducteePoint {
   user: number;
-  name: string;
   email: string;
   points: number;
   hasProfessionalRequirement: string;
@@ -21,8 +19,7 @@ export async function getAllInducteePoints(): Promise<InducteePoint[]> {
   const pointsApi: PointsApi = new PointsApi(apiConfig);
 
   const points = await pointsApi.pointsControllerGetAllInducteePoints();
-  const inducteePointsList = await Promise.all(points.inducteePoints.map(async (point: InducteePointsResponse) => {
-    const userAcc = await getUserById(point.user);
+  return points.inducteePoints.map((point: InducteePointsResponse) => {
     return {
       points: point.points,
       user: point.user,
@@ -39,8 +36,6 @@ export async function getAllInducteePoints(): Promise<InducteePoint[]> {
       hasSocialRequirement: point.hasSocialRequirement
         ? 'Complete'
         : 'Incomplete',
-      name: userAcc.firstName + " " + userAcc.lastName,
     } as InducteePoint;
-  }));
-  return inducteePointsList;
+  });
 }
