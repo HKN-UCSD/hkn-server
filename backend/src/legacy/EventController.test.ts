24aec4ca1d363e6770f7a1027f1c389bf4798070
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2022-09-29 11:23:51 -0700

    Created legacy folder and moved failed test

---
+++ backend/src/legacy/EventController.test.ts
@@ -0,0 +1,38 @@
+// This test has been failing ever since after May 3rd, 2022. This will be commented out for deployment purposes
+
+// It has been removed as of 9/29/2022 by Kyle Wade
+
+import request from 'supertest';
+import { Application } from 'express';
+import { getExpressApp } from '../app';
+import { Connection } from 'typeorm';
+
+let testApp: Application = null;
+let testConnection: Connection = null;
+
+beforeAll(async () => {
+  const { app, connection } = await getExpressApp();
+
+  testApp = app;
+  testConnection = connection;
+});
+
+afterAll(async () => {
+  await testConnection.close();
+});
+
+describe('GET /api/events', () => {
+  it('tests if GET request to /api/events can be made without crashing the server.', async () => {
+    const res = await request(testApp).get('/api/events');
+
+    expect(res.status).toEqual(200);
+  });
+});
+
+describe('GET /api/events/:eventID', () => {
+  it('tests if GET request to /api/events/:eventID with some hardcoded eventID can be made without crashing the server.', async () => {
+    const res = await request(testApp).get('/api/events/2');
+
+    expect(res.status).toEqual(200);
+  });
+});
