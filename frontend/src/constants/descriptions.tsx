a78f46930c9bde0ca83728beccdfdfb9b78e33d7
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2022-09-29 13:54:28 -0700

    Commented unused import

--- frontend/src/constants/descriptions.tsx
+++ frontend/src/constants/descriptions.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
 
-import { Link } from '@SharedComponents';
+// import { Link } from '@SharedComponents';
 
 export const DescriptionText = () => (
   <Typography>
