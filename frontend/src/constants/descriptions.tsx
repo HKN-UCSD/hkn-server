00424cce211d4c969dbe58601aa29299a5952167
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2022-09-29 16:03:24 -0700

    Updated zoom link

--- frontend/src/constants/descriptions.tsx
+++ frontend/src/constants/descriptions.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
 
-// import { Link } from '@SharedComponents';
+import { Link } from '@SharedComponents';
 
 export const DescriptionText = () => (
   <Typography>
     Info Session 1: Friday, Oct. 7th at 5PM <br />
     Info Session 2: Tuesday, Oct. 11th at 4PM <br />
-    Location: Zoom (Check invite email for link) <br />
-    {/* Zoom Link: <Link
-      href='https://ucsd.zoom.us/j/91836877195'
+    {/* Location: Zoom (Check invite email for link) <br /> */}
+    Zoom Link: <Link
+      href='https://ucsd.zoom.us/j/94648277244'
       openInNewTab
     />{' '}
-    <br /> */}
+    <br />
   </Typography>
 );
