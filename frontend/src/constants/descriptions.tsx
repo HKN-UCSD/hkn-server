f1a1edc17c1c5f30257e5abe51b195774abc7afd
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2022-12-11 21:26:24 -0800

    Updated info sessions

--- frontend/src/constants/descriptions.tsx
+++ frontend/src/constants/descriptions.tsx
@@ -5,8 +5,8 @@ import { Link } from '@SharedComponents';
 
 export const DescriptionText = () => (
   <Typography>
-    Info Session 1: Friday, Oct. 7th at 7PM <br />
-    Info Session 2: Tuesday, Oct. 11th at 4PM <br />
+    Info Session 1: TBD <br />
+    Info Session 2: TBD <br />
     {/* Location: Zoom (Check invite email for link) <br /> */}
     Zoom Link: <Link
       href='https://ucsd.zoom.us/j/94648277244'
